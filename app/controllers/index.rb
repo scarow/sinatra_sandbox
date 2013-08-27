1ee453a2218f3cda85c224803720c9d131721639
Apprentice <apprentice@dbc15.local>
2013-08-27 12:01:19 -0700

    first commit

---
+++ app/controllers/index.rb
@@ -0,0 +1,22 @@
+
+
+get '/' do
+  puts "[LOG] Getting /"
+  puts "[LOG] Params: #{params.inspect}"
+  erb :index
+end
+
+
+post '/cool_url' do
+  p params
+  puts "[LOG] Posting to /cool_url"
+  puts "[LOG] Params: #{params.inspect}" 
+  erb :post_cool_url
+end
+
+get '/cool_url' do
+  puts "[LOG] Getting /cool_url"
+  puts "[LOG] Params: #{params.inspect}"
+  erb :get_cool_url
+end
+
